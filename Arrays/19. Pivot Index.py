fac677d511b2713fe05d33e04620b48367192059
AKRISHT-VERMA <akrishtverma05@gmail.com>
2026-07-03 22:22:51 +0530

    feat: add optimal solution implementation for Pivot Index algorithm

--- Arrays/19. Pivot Index.py
+++ Arrays/19. Pivot Index.py
@@ -1,3 +1,4 @@
+'''Method - 1 Brute force '''
 arr = [1, 7, 3, 6, 5, 6]
 left_sum = 0
 right_sum = 0
@@ -7,3 +8,19 @@ for i in range(len(arr)):
     if left_sum == right_sum:
         print("The pivot index is :" , i)
         break
+
+'''Method - 2 --- Optimal Solution'''
+arr = [1, 7, 3, 6, 5, 6]
+left_sum = 0
+right_sum = 0
+found = False
+total_sum = sum(arr)
+for i in range(len(arr)):
+    right_sum = total_sum - (left_sum + arr[i])
+    if left_sum == right_sum:
+        print(i)
+        found = True
+        break
+    left_sum += arr[i]
+if not found:
+    print(-1)
