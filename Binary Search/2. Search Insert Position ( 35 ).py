e310f4693b5cbdeb9bf4424fe273ca768ce49f37
AKRISHT-VERMA <akrishtverma05@gmail.com>
2026-07-18 15:44:15 +0530

    refactor: adjust testing block to ensure proper execution context

--- Binary Search/2. Search Insert Position ( 35 ).py
+++ Binary Search/2. Search Insert Position ( 35 ).py
@@ -17,9 +17,10 @@ class Solution:
         return  low        
     
 #Testing
-sol = Solution()
+if __name__ == "__main__":
+      sol = Solution()
 
-print(sol.searchInsert([1,3,5,6], 5))   
-print(sol.searchInsert([1,3,5,6], 2))   
-print(sol.searchInsert([1,3,5,6], 7))  
-print(sol.searchInsert([1,3,5,6], 0))    
+      print(sol.searchInsert([1,3,5,6], 5))   
+      print(sol.searchInsert([1,3,5,6], 2))   
+      print(sol.searchInsert([1,3,5,6], 7))  
+      print(sol.searchInsert([1,3,5,6], 0))    
